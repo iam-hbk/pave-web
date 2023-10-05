eda880466416bbbe0a25ebfa29aff021c0c6b18e
iam-hbk <rainheritier@gmail.com>
2023-10-05 13:23:34 +0200

    updated Welcome component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Urbanist } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Urbanist({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Pave',
