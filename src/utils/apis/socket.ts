04d02e9109acf304e3726443ba3d161694965eb2
Heritier Kaumbu <rainheritier@gmail.com>
2023-10-12 21:32:09 +0200

    Update socket.ts

--- src/utils/apis/socket.ts
+++ src/utils/apis/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const serverUrl = "http://localhost:4001";
+const serverUrl = "https://pave-server.onrender.com";
 
 let socket: Socket | null = null;
 
